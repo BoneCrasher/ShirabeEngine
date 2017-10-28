70d311fb3abba3b338d2b40ccdf40dca033ba313
Marc-Anton Boehm-von Thenen <marc-anton.boehm-von-thenen_ext@smartray.de>
2017-10-28 17:12:03 +0200

    ../.. blocked

--- ShirabeEngine/code/include/Resources/Subsystems/GFXAPI/GFXAPIResourceSubsystem.h
+++ ShirabeEngine/code/include/Resources/Subsystems/GFXAPI/GFXAPIResourceSubsystem.h
@@ -22,16 +22,54 @@ namespace Engine {
 	namespace GFXAPI {
 		using namespace Engine::Resources;
 
+		/**********************************************************************************************//**
+		 * \enum	EGFXAPI
+		 *
+		 * \brief	Identifies a specific graphics API
+		 **************************************************************************************************/
 		enum class EGFXAPI {
 			DirectX = 1,
 			OpenGL  = 2,
 			Vulkan  = 4
 		};
 
+		/**********************************************************************************************//**
+		 * \enum	EGFXAPIVersion
+		 *
+		 * \brief   Identifies a specific graphics API version requirement
+		 **************************************************************************************************/
+		enum class EGFXAPIVersion {
+			DirectX_11_0,
+			DirectX_11_1,
+			DirectX_11_2,
+			DirectX_12_0,
+			OpenGL_,
+			Vulkan_
+		};
+
+		enum class ERequestMode {
+			Async = 1,
+			Sync  = 2
+		};
+
+		/**********************************************************************************************//**
+		 * \struct	DeferredResourceCreationHandle
+		 *
+		 * \brief	Handle class to hold an asynchronous request.
+		 **************************************************************************************************/
 		struct DeferredResourceCreationHandle {
 			std::future<GFXAPIResourceHandle_t>& futureHandle;
 		};
 
+		/**********************************************************************************************//**
+		 * \class	GFXAPIResourceSubSystem
+		 *
+		 * \brief	The GFXAPIResourceSubSystem is responsible for spawning a resource task based on 
+		 * 			 the specific request, executing it (a-)synchronuously and store the results.
+		 * 			The task itself will be spawned by a TaskBuilder, determined during runtime. 
+		 * 			The subsystem holds a mapping of GFXAPIResourceHandle_t to GFXAPIResourceHolder to 
+		 * 			 store and manage the resources.
+		 **************************************************************************************************/
 		class GFXAPIResourceSubSystem {		
 			DeclareLogTag(GFXAPIResourceSubSystem);
 		public:
@@ -47,15 +85,18 @@ namespace Engine {
 			DeclareInterfaceEnd(IAsyncLoadCallback);
 
 			template <typename TResource>
-			EEngineStatus loadSync(
-				const ResourceDescriptor<TResource> &descriptor,
-				GFXAPIResourceHandleMap             &inResourceDependencyHandles,
+			EEngineStatus load(
+				const ResourceDescriptor<TResource> &inDescriptor,
+				const GFXAPIResourceHandleMap       &inResourceDependencyHandles,
+				const ERequestMode                  &inRequestMode,
+				const Ptr<IAsyncLoadCallback>       &inCallback,
 				GFXAPIResourceHandle_t              &outResourceHandle);
 
 			template <typename TResource>
-			EEngineStatus loadAsync(
-				const ResourceDescriptor<TResource> &descriptor,
-				Ptr<IAsyncLoadCallback> &callback);
+			EEngineStatus unload(
+				GFXAPIResourceHandle_t &outResourceHandle,
+				const ERequestMode     &inRequestMode);
+
 
 		private:
 			template <typename TResource>
@@ -79,45 +120,51 @@ namespace Engine {
 			Threading::Looper<
 		};
 
-
+		/**********************************************************************************************//**
+		 * \fn	template <typename TResource> EEngineStatus GFXAPIResourceSubSystem::load( const ResourceDescriptor<TResource> &inDescriptor, const GFXAPIResourceHandleMap &inResourceDependencyHandles, const ERequestMode &inRequestMode, const Ptr<IAsyncLoadCallback> &inCallback, GFXAPIResourceHandle_t &outResourceHandle)
+		 *
+		 * \brief	Loads
+		 *
+		 * \tparam	TResource	Type of the resource.
+		 * \param 		  	inDescriptor			   	Information describing the in.
+		 * \param 		  	inResourceDependencyHandles	The in resource dependency handles.
+		 * \param 		  	inRequestMode			   	The in request mode.
+		 * \param 		  	inCallback				   	The in callback.
+		 * \param [in,out]	outResourceHandle		   	Handle of the out resource.
+		 *
+		 * \return	The EEngineStatus.
+		 **************************************************************************************************/
 		template <typename TResource>
-		EEngineStatus GFXAPIResourceSubSystem::loadSync(
-			const ResourceDescriptor<TResource> &descriptor,
-			GFXAPIResourceHandleMap             &inResourceDependencyHandles,
+		EEngineStatus GFXAPIResourceSubSystem::load(
+			const ResourceDescriptor<TResource> &inDescriptor,
+			const GFXAPIResourceHandleMap       &inResourceDependencyHandles,
+			const ERequestMode                  &inRequestMode,
+			const Ptr<IAsyncLoadCallback>       &inCallback,
 			GFXAPIResourceHandle_t              &outResourceHandle)
 		{
 			DeferredResourceCreationHandle handle;
 
+			GFXAPIResourceHandle_t resourceHandle = 0;
+
 			EEngineStatus status = loadImpl(descriptor, inResourceDependencyHandles, handle);
 			if( !CheckEngineError(status) ) {
-				GFXAPIResourceHandle_t resourceHandle = handle.futureHandle.get(); // Wait for it...
-				if( resourceHandle == 0 )
-					// Invalid
-					status = EEngineStatus::GFXAPI_SubsystemResourceCreationFailed;
-				else {
-					outResourceHandle = resourceHandle;
-					status = EEngineStatus::Ok;
+				switch( inRequestMode ) {
+				default:
+				case ERequestMode::Sync:
+
+					resourceHandle = handle.futureHandle.get(); // Wait for it...
+					if( resourceHandle == 0 )
+						// Invalid
+						status = EEngineStatus::GFXAPI_SubsystemResourceCreationFailed;
+					else {
+						outResourceHandle = resourceHandle;
+						status = EEngineStatus::Ok;
+					}
+					break;
+				case ERequestMode::Async:
+					break;
 				}
-			}
-
-			return status;
-		}
-
-		template <typename TResource>
-		EEngineStatus GFXAPIResourceSubSystem::loadAsync(
-			const ResourceDescriptor<TResource> &descriptor,
-			Ptr<IAsyncLoadCallback> &callback)
-		{
-			DeferredResourceCreationHandle handle;
-
-			EEngineStatus status = loadImpl(descriptor, handle);
-			if( !CheckEngineError(status) ) {
-				// Won't be further implemented for now!!
-				// If continued:
-				//   Create task UID
-				//   Store handle w/ future and callback together
-				//   Have separate timed task run in intervals to verify the resource states and invoke the callbacks, where necessary.
-			}
+			}				
 
 			return status;
 		}
@@ -138,7 +185,7 @@ namespace Engine {
 			}
 
 			std::shared_future<GFXAPIResourceHandle_t> future;
-			status = enqueue<TResource>(builder, future);
+			status = enqueue<TResource>(task, future);
 			if( CheckEngineError(status) ) {
 				Log::Error(logTag(), String::format("Failed to enqueue resource creation task for resource '%0'", descriptor.name()));
 				return status;
