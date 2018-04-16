ed042ae13cbb22b783d5327d4026a0d124fc4739
Marc-Anton Boehm-von Thenen <marc-anton.boehm-von-thenen_ext@smartray.de>
2018-04-16 19:51:03 +0200

    Moved external libraries to...

--- ShirabeEngine/ext/libxslt/Linux64/Debug/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/dotti/SmartRay/sr_3rdparty_sources/libXSLT/deploy/Linux64/Debug/lib"
-XSLT_LIBS="-lxslt -L/home/dotti/SmartRay/sr_3rdparty_sources/libXSLT/../libXML2/deploy/Linux64/Debug/lib -L/home/dotti/SmartRay/sr_3rdparty_sources/libXML2/deploy/Linux64/Debug/lib -lxml2 -L/home/dotti/SmartRay/sr_3rdparty_sources/libXML2/../zlib/deploy/Linux64/Debug/lib -lz -lm -lm"
-XSLT_INCLUDEDIR="-I/home/dotti/SmartRay/sr_3rdparty_sources/libXSLT/deploy/Linux64/Debug/include"
-MODULE_VERSION="xslt-1.1.26"
