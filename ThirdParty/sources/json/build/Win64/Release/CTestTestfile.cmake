97758bce4a28011660d20c37e95be842ea0115d2
Marc-Anton Boehm-von Thenen <marc-anton.boehm-von-thenen_ext@smartray.de>
2018-04-16 20:05:17 +0200

    Clean binaries

--- ThirdParty/sources/json/build/Win64/Release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/ShirabeDevelopment/ShirabeEngine/ext/sources/json/json_master
-# Build directory: F:/ShirabeDevelopment/ShirabeEngine/ext/sources/json/build/Win64/Release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
