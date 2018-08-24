e3ad2b9d33ec518025b59a4f474b271bd4f3bbe3
Marc-Anton Boehm-von Thenen <boehm.marc.anton@gmail.com>
2018-08-24 18:16:29 +0200

    [SH-1] Implemented window creation in factory

--- shirabeengine/shirabeengine/modules/wsi/code/source/x11/x11windowfactory.cpp
+++ shirabeengine/shirabeengine/modules/wsi/code/source/x11/x11windowfactory.cpp
@@ -34,6 +34,21 @@ namespace engine
 
                 CStdSharedPtr_t<IWindow> window = makeCStdSharedPtr<CX11Window>(aName, aInitialBounds);
 
+                Display *const display = reinterpret_cast<Display *const>(mX11Display->displayHandle());
+                uint64_t const x       = aInitialBounds.position.x();
+                uint64_t const y       = aInitialBounds.position.y();
+                uint64_t const w       = aInitialBounds.size.x();
+                uint64_t const h       = aInitialBounds.size.y();
+
+                try
+                {
+                    Window const x11Window = XCreateWindow(display, 0, x, y, w, h, 1, CopyFromParent, InputOutput, nullptr, 0, nullptr);
+                }
+                catch(...)
+                {
+                    // TBDone: Log
+                    return 0;
+                }
 
                 return window;
             }
